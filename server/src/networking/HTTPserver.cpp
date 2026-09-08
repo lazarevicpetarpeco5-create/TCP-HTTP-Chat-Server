60f97337bc7fc3889869762af566faa3259e35e4
Petar Lazarević <lazarevicpetarpeco5-create@users.noreply.github.com>
2026-09-08 18:55:41 -0400

    Reorganized Http File made it more capsualted than TCP part of server

--- server/src/networking/HTTPserver.cpp
+++ server/src/networking/HTTPserver.cpp
@@ -1,23 +1,71 @@
 #include "HttpServer.hpp"
-
 #include <iostream>
 #include <string>
 #include <sstream>
-
+#include <thread>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <unistd.h>
 
+int HttpServer::Start() {
 
-   
+    sockaddr_in ServerAdress{};
+
+    ServerAdress.sin_family = (AF_INET);
+    ServerAdress.sin_port = htons(8080);
+    ServerAdress.sin_addr.s_addr = INADDR_ANY;
+
+
+
+  int HttpSerSocket = socket(AF_INET, SOCK_STREAM, 0);
+  if (HttpSerSocket < 0)   
+  {
+   std::cerr << "[HTTP] socket() creation failed\n";
+    return -1;
+  } 
+  
+    if (bind(HttpSerSocket,
+    reinterpret_cast<sockaddr*>(&ServerAdress),
+    sizeof(ServerAdress)
+    ) == -1 )
+    {
+    std::cerr << "bind() failed\n";
+    return 1;
+    }
+
+   if (listen(HttpSerSocket, 5) == -1)
+   {
+    std::cerr << "listen() failed\n";
+    return 1;
+   }
+   return HttpSerSocket;
+}
+
+
+void HttpServer::AcceptLoop(int HttpSerSocket){
+
+while(true)
+  { 
+    ///Blocks connection until a client is connected 
+    int HttpClient = accept(HttpSerSocket, )
+     
+    }
+
+  if (clientsocket < 0) 
+    {
+    std::cerr << "[HTTP] accept() failed\n";
+    continue;
+    }
+}
 
 
 
 
 void HttpServer:: HandleRequest(int clientsocket)  {
-
 char buffer[4096];
-
 int bytesrecived = recv(clientsocket, buffer, sizeof(buffer) - 1, 0);
+
+
 
 if (bytesrecived == -1)
 {
@@ -31,7 +79,6 @@ if (bytesrecived == 0)
 }
 
 
-    buffer[bytesrecived] = '\0';
-
+buffer[bytesrecived] = '\0';
 std::cout << buffer << std::endl;
 } 
--- server/src/networking/HTTPserver.hpp
+++ server/src/networking/HTTPserver.hpp
@@ -4,6 +4,10 @@
 class HttpServer
 {
   public:
-        void HandleRequest(int clientsocket);
-
+  // Creates, binds, and listens on port 8080 (returns the server socket FD)
+     int Start();
+  // Infinite loop that accepts client sockets and spawns threads
+     void  AcceptLoop(int HttpSerSocket);
+  // Handles the individual client socket  
+     void HandleRequest(int clientsocket);
 };
